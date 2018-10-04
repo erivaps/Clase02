4a1a59a292e6710b50f2d64737b2cf80a5b35b8c
Erison <erivaps@gmail.com>
2018-10-03 22:58:49 -0500

    first commit

---
+++ app/src/main/java/com/kotlin/miprimeraapp/clase02/Jugador.kt
@@ -0,0 +1,12 @@
+package com.kotlin.miprimeraapp.clase02
+
+class Jugador {
+    companion object {
+        val nombre:String = "Erison"
+    }
+}
+
+
+fun main (args: Array<String>){
+    println(Jugador.nombre)
+}
