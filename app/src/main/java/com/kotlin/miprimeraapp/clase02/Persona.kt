efc692d2070549fd654c1de3e42b74cfc5ea260d
Erison <erivaps@gmail.com>
2018-10-03 21:35:40 -0500

    first commit

---
+++ app/src/main/java/com/kotlin/miprimeraapp/clase02/Persona.kt
@@ -0,0 +1,18 @@
+package com.kotlin.miprimeraapp.clase02
+//clase abstracta
+abstract class Persona {
+
+    abstract fun correr()
+    abstract fun saltar()
+
+}
+
+class Hombre: Persona(){
+    override fun correr() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun saltar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
